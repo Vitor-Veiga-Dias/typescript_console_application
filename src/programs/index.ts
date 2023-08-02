9bea0cb62c788477fc0838e9e3015b520c46214f
Vitor-Veiga-Dias <vitorveigadias@gmail.com>
2023-08-01 23:48:26 -0300

    chore:add index for export all programs

---
+++ src/programs/index.ts
@@ -0,0 +1,8 @@
+export { startCalculator } from './calculator'
+export { startPimeNumbers } from './prime-numbers';
+export { startFactorial } from './factorial';
+export { startisPalindrome } from './palindrome';
+export { startdisplayMultiplicationTable } from './table';
+export { startcountVowels } from './vowel-counter';
+export { startCalculateAverage } from './grade-average';
+export { startCalculateFinalValue } from './interest-calculation';
